f7dc467979ff0ce0bd0ed18d5b644a6c12bb64c9
Cortney Gobern <CortneyGobern@Cortneys-MacBook-Pro.local>
2016-08-31 17:39:01 -0400

    Initial commit

---
+++ liri.js
@@ -0,0 +1,10 @@
+var fs = require('fs');
+
+var twitter = require('twitter');
+var request = require('request');
+
+//requiring the twitter keys
+var twitterKeys = require('./keys.js');
+console.log(twitterKeys);
+
+//use switches when writing your code
