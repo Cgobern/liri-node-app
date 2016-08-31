775a2295efe605d2e6a187da6e1995bbdc63911b
Cortney Gobern <CortneyGobern@Cortneys-MacBook-Pro.local>
2016-08-31 18:15:44 -0400

    added twitter keys

--- keys.js
+++ keys.js
@@ -1,8 +1,8 @@
 console.log('this is loaded');
 
 exports.twitterKeys = {
-  consumer_key: '<input here>',
-  consumer_secret: '<input here>',
-  access_token_key: '<input here>',
-  access_token_secret: '<input here>',
+  consumer_key: '<vZmA2pXOi46IE0rXKepix3m0C>',
+  consumer_secret: '<QKcaH7IyPfdKgPvbNr05q2fleP85JxORhSpctG7RSiyv68ACAB>',
+  access_token_key: '<771103423706136577-S0txW3L2f2hSTxX5ILWXtKq1eYH0ssh>',
+  access_token_secret: '<rVuH1c6jwfDOPejWMcu5pZ9RvSu9ctNPGN5rS7DopA8CZ>',
 }
